f676bea97d3196920ac438118f39a9c33084f44f
lysshochberger <59520545+lysshochberger@users.noreply.github.com>
2020-04-21 18:33:28 -0400

    added more files from django file

---
+++ django/django/contrib/postgres/lookups.py
@@ -0,0 +1,69 @@
+from django.db.models import Transform
+from django.db.models.lookups import Exact, PostgresOperatorLookup
+
+from .search import SearchVector, SearchVectorExact, SearchVectorField
+
+
+class DataContains(PostgresOperatorLookup):
+    lookup_name = 'contains'
+    postgres_operator = '@>'
+
+
+class ContainedBy(PostgresOperatorLookup):
+    lookup_name = 'contained_by'
+    postgres_operator = '<@'
+
+
+class Overlap(PostgresOperatorLookup):
+    lookup_name = 'overlap'
+    postgres_operator = '&&'
+
+
+class HasKey(PostgresOperatorLookup):
+    lookup_name = 'has_key'
+    postgres_operator = '?'
+    prepare_rhs = False
+
+
+class HasKeys(PostgresOperatorLookup):
+    lookup_name = 'has_keys'
+    postgres_operator = '?&'
+
+    def get_prep_lookup(self):
+        return [str(item) for item in self.rhs]
+
+
+class HasAnyKeys(HasKeys):
+    lookup_name = 'has_any_keys'
+    postgres_operator = '?|'
+
+
+class Unaccent(Transform):
+    bilateral = True
+    lookup_name = 'unaccent'
+    function = 'UNACCENT'
+
+
+class SearchLookup(SearchVectorExact):
+    lookup_name = 'search'
+
+    def process_lhs(self, qn, connection):
+        if not isinstance(self.lhs.output_field, SearchVectorField):
+            config = getattr(self.rhs, 'config', None)
+            self.lhs = SearchVector(self.lhs, config=config)
+        lhs, lhs_params = super().process_lhs(qn, connection)
+        return lhs, lhs_params
+
+
+class TrigramSimilar(PostgresOperatorLookup):
+    lookup_name = 'trigram_similar'
+    postgres_operator = '%%'
+
+
+class JSONExact(Exact):
+    can_use_none_as_rhs = True
+
+    def process_rhs(self, compiler, connection):
+        result = super().process_rhs(compiler, connection)
+        # Treat None lookup values as null.
+        return ("'null'", []) if result == ('%s', [None]) else result
