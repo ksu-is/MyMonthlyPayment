9c02c1041bc553486d0bd0f90c2ecf510f68284e
lysshochberger <59520545+lysshochberger@users.noreply.github.com>
2020-04-22 21:37:09 -0400

    added selenium and login form

--- MyMonthlyPayment.py
+++ MyMonthlyPayment.py
@@ -1,15 +1,37 @@
 
-#requesting users login information 
-print("Here we will need the link to the login page on the website you make payments on ")
-url = input("Enter the url of the login page: ")
-username = input("Username used to login: ")
-password = input("Password used to login: ")
-# >>add submit<< 
+from selenium import webdriver
+driver = webdriver.Chrome()
+from selenium.webdriver.common.keys import Keys
 
-#using user input, website is requested
-import requests
-from bs4 import BeautifulSoup
-page = requests.get(url)
+chromedriver = 'D:\\chromedriver.exe'
+page = webdriver.Chrome(chromedriver)
+# request user login information and automatically enter information 
+print("Here we will need the link to the login page on the website you make payments on ")
+website_url = input("Enter the url of the login page: ")
+driver.get(website_url)
+user_id = input("Username used to login: ")
+site_username = driver.find_element_by_id(user_id)
+site_username.send_keys(user_username)
+user_password = input("Password used to login: ")
+site_password = driver.find_element_by_id("password")
+password = selenium.send_keys(user_password)
+driver.find_element_by_id("Login").submit()
+authentication = "hhtp://" + user_username + user_password + website_url
+driver.get(authentication + "/")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #>>try later<<
 #import forms from django
