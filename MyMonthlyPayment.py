eb56745edc9ab48a711df4c2ec89d2e46f8bbdf1
lysshochberger <59520545+lysshochberger@users.noreply.github.com>
2020-04-28 12:32:51 -0400

    fixed chromedriver location added comments and spacing

--- MyMonthlyPayment.py
+++ MyMonthlyPayment.py
@@ -1,53 +1,36 @@
 
 from selenium import webdriver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path = r"C:\\Users\\miley\\OneDrive- Kennesaw State University\\" +
+    "Spring '20\\App Dev\\ProjectDemos\\Project Topic\\test\\chromedriver_win32")
 from selenium.webdriver.common.keys import Keys
 
-chromedriver = 'D:\\chromedriver.exe'
+chromedriver = r"C:\\Users\\miley\\OneDrive- Kennesaw State University\\" +
+   "Spring '20\\App Dev\\ProjectDemos\\Project Topic\\test\\chromedriver_win32"
 page = webdriver.Chrome(chromedriver)
+
+
 # request user login information and automatically enter information 
 print("Here we will need the link to the login page on the website you make payments on ")
+
+# User enters the website URL
 website_url = input("Enter the url of the login page: ")
 driver.get(website_url)
+
+# User enters their username for the website URL provided prior
 user_id = input("Username used to login: ")
-site_username = driver.find_element_by_id(user_id)
-site_username.send_keys(user_username)
+site_user = driver.find_element_by_id(user_id)
+site_user.send_keys(user_id)
+
+# User enters their password 
 user_password = input("Password used to login: ")
 site_password = driver.find_element_by_id("password")
 password = selenium.send_keys(user_password)
+
+# submit login
 driver.find_element_by_id("Login").submit()
+
+# login authentication
 authentication = "hhtp://" + user_username + user_password + website_url
 driver.get(authentication + "/")
 
 
-from bs4 import BeautifulSoup
-import requests
-import csv
-# (url needed)
-soup = BeautifulSoup(source, 'lxml')
-payment = soup.find_all('due')
-total_payments = int("payment") + 
-
-
-total_due = print("$", str(total_payments))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#>>try later<<
-#import forms from django
-#class URLInput:
-#    input_type: 'url'
-#    template_name: 'django/forms/widgets/url.html'
