c0ff2fe7436600cd7b3ffcb6a2460fef15d791d9
lysshochberger <59520545+lysshochberger@users.noreply.github.com>
2020-04-28 20:08:33 -0400

    made interactive login function, collect data, total numbers

--- app/MyMonthlyPayment.py
+++ app/MyMonthlyPayment.py
@@ -1,4 +1,4 @@
-
+import selenium
 from selenium import webdriver
 driver = webdriver.Chrome(executable_path = r"C:\\Users\\miley\\OneDrive- Kennesaw State University\\" +
     "Spring '20\\App Dev\\ProjectDemos\\Project Topic\\test\\chromedriver_win32\\chromedriver.exe")
@@ -18,19 +18,39 @@ driver.get(website_url)
 
 # User enters their username for the website URL provided prior
 user_id = input("Username used to login: ")
-site_user = driver.find_element_by_id(user_id)
-site_user.send_keys(user_id)
+username = driver.find_element_by_xpath("//form[@id = user_id]/input[1]")
+username.send_keys(user_id)
 
 # User enters their password 
-user_password = input("Password used to login: ")
-site_password = driver.find_element_by_id("password")
-password = selenium.send_keys(user_password)
+password = input("Password used to login: ")
+site_password = driver.find_element_by_xpath("//form[@name = password]/input[1]")
+site_password.send_keys(site_password)
 
 # submit login
-driver.find_element_by_id("Login").submit()
+login = driver.find_element_by_xpath('continue').submit()
 
 # login authentication
-authentication = "hhtp://" + user_id + user_password + website_url
+authentication = "hhtp://" + user_id + password + website_url
 driver.get(authentication + "/")
+
+
+# Wait
+driver.implicitly_wait(10) #seconds
+
+# List of bill payments due
+bills_due = []
+
+#adding amounts due to list and totaling payments together
+amount_search = driver.find_element_by_xpath('amountDue')
+
+bills_due.append(amount_search)
+total_due = sum(bills_due)
+
+
+print("$", total_due)
+
+
+
+
 
 
